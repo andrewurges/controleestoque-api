7a1f17ad129ad142798cb48daa9a4e00daa23a70
André Ricardo Würges <ar.wurges@gmail.com>
2021-09-23 15:29:57 -0300

    Alterando ToDo para Pedido e ajustes no EstoqueController

---
+++ ControleEstoque.Data/Enum/ESituacaoPagamento.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace ControleEstoque.Data.Enum
+{
+    public enum ESituacaoPagamento
+    {
+        [Description("Pendente")] Pendente = 0,
+        [Description("Pago")] Pago = 1
+    }
+}
---
+++ ControleEstoque.Data/Enum/ESituacaoPedido.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace ControleEstoque.Data.Enum
+{
+    public enum ESituacaoPedido
+    {
+        [Description("A fazer")] AFazer = 0,
+        [Description("Em produção")] EmProducao = 1,
+        [Description("Finalizado")] Finalizado = 2
+    }
+}
