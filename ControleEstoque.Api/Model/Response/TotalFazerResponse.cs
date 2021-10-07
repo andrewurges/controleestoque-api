020e472b1f37e7996c7e0be4c9495493a7628721
André Ricardo Würges <ar.wurges@gmail.com>
2021-10-07 11:21:50 -0300

    Correção no retorno do total de pedidos a fazer

---
+++ ControleEstoque.Api/Model/Response/TotalFazerResponse.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace ControleEstoque.Api.Model.Response
+{
+    public class TotalFazerResponse
+    {
+        [JsonProperty("quantidade")]
+        public int Quantidade { get; set; }
+
+        [JsonProperty("pedidos")]
+        public IEnumerable<object> Pedidos { get; set; }
+    }
+}
